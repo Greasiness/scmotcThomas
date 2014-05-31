fd89b84b679d383e74a25fb15a6ab3929cee8987
thomaschang <thc005@ucsd.edu>
2014-05-30 18:23:43 -0700

    added shit

--- app.js
+++ app.js
@@ -3,26 +3,41 @@ var express = require('express');
  var   mongoose = require('mongoose');
   var  http = require('http');
   var  connect = require('connect');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(connect.bodyParser());       // to support JSON-encoded bodies
 app.use(connect.urlencoded()); // to support URL-encoded bodies
-
+app.use(cookieParser())
+app.use(express.session({
+    secret: 'thomasSecretKeyDontTellAnyone'
+    , store: express.session.MemoryStore({
+        reapInterval: 60000 * 10
+    })
+}));
 
 var userSchema = new mongoose.Schema({
     useremail: String,
     password: String
 });
 
-var user = mongoose.model('users', userSchema);
+var applicationSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    GPA: String,
+    useremail: String
+});
 
+var user = mongoose.model('users', userSchema);
+var application = mongoose.model('applications', applicationSchema);
 //my heroku mondodb
 mongoose.connect('mongodb://heroku:K_KZx326E9XPIwy_9S24NNGcVfQlcWL2KdaPe3BJh19sGT_-ef1M94p5LKRIDBv3K9d_HIPzqtem0tydEcHYMg@paulo.mongohq.com:10045/app18634011');
 
-require('./routes/routes')(app, user);
+require('./routes/routes')(app, user, application);
 
 
-http.createServer(app).listen(3000, function() {
+http.createServer(app).listen(3002, function() {
     console.log('Express server listening on port ');
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,15 @@
-module.exports = function(app, user){
+module.exports = function(app, user, application){
 app.get('/', function(req, res){
+    if(req.session.useremail) {
+        res.redirect('/home');
+        console.log(req.session.useremail);
+        return;
+    }
     res.render('index.html');
 });
 
 
-app.post('/DBinsert', function(req, res){
+app.post('/register', function(req, res){
     var temp = new user({
         useremail: req.body.useremail,
         password: req.body.password
@@ -12,11 +17,10 @@ app.post('/DBinsert', function(req, res){
     console.log("before save");
     temp.save(function (err) {if (err) console.log ('Error on save!')});
     console.log("saved " + req.body.useremail )  ;
-    res.redirect('/db');
+    res.redirect('/');
 })  ;
 
 app.get('/db', function(req,res){
-    console.log('in route')
     user.find(function(err, users) {
         if (err) return console.error(err);
         res.send(users);
@@ -27,13 +31,83 @@ app.get('/login', function(req,res){
     res.render('login.html');
 });
 
-app.post('/logindb', function(req,res){
-    user.find({ email: req.body.useremail, password: req.body.password },function(err, users) {
+app.get('/register', function(req, res){
+    res.render('register.html');
+});
+
+app.get('/home', function(req, res){
+    user.find({useremail: req.session.useremail}, function(err, user){
+        if(err || user.length == 0){
+            res.render('loginfailed.html');
+            return;
+        }
+        else
+        application.find({useremail: req.session.useremail}, function(err, application){
+            if(err||application.length== 0){
+                res.render("homepage.html")
+            } else
+            {
+                res.setHeader('Content-Type', 'text/html');
+                res.write('<html><head></head>');
+                res.write('<body>');
+                var temp = '<p>' + application[0].firstname + '</p>'
+                res.write(temp);
+                temp = '<p>' + application[0].lastname + '</p>';
+                res.write(temp);
+                temp = '<p>' + application[0].GPA + '</p>';
+                res.write(temp);
+                res.write('<br><br>');
+                res.write('<form action = "/reapply" method="POST">');
+
+                res.write('<input type ="submit" value="Resubmit">');
+                res.write('</form></body></html>');
+                res.end;
+            }
+
+        });
+    })
+}) ;
+
+app.get('/logout', function(req,res){
+    req.session.destroy();
+    res.redirect('/');
+})
+
+app.post('/login', function(req,res){
+    user.find({ useremail: req.body.useremail, password: req.body.password },function(err, users) {
         if (err || users.length == 0){
-            res.send("Not Today");
+            res.render('loginfailed.html');
             return console.error(err);
         }
-        res.send("Today");
+        req.session.useremail = users[0].useremail;
+        res.redirect('/home');
     });
 });
+
+app.post('/apply', function(req,res){
+    var temp = new application({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        GPA: req.body.GPA,
+        useremail: req.session.useremail
+    });;
+    temp.save(function (err) {if (err) console.log ('Error on save!')});
+    console.log("saved " + req.session.useremail + " application")  ;
+    res.redirect('/home');
+});
+
+app.post('/reapply', function(req,res){
+    application.find({useremail: req.session.useremail}, function(err, application){
+        if(err||application.length== 0){
+            res.redirect('/home');
+            return;
+        } else
+        {
+          application[0].remove();
+            res.redirect('/home');
+            return;
+        }
+    });
+
+    });
 };
